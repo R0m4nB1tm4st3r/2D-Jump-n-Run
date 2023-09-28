0897fb7169ad72a614e31d3b31eceede0a75de5d
Roman Hochhalter <roman.Hochhalter@talidu.com>
2023-09-28 15:13:35 +0200

    add basic movement mechanics

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+[RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] 
+        float moveSpeed = 10, jumpStrength = 9;
+	[SerializeField]
+	    private Rigidbody2D rigidBody = null;
+
+	bool isOnGround;
+    float xDelta, yDelta; 
+    Vector2 newDirection;
+    @_2DJumpnRun inputAction = null;
+
+	private void Awake()
+	{
+		inputAction = new();
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        if (rigidBody == null) rigidBody = GetComponent<Rigidbody2D>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		Vector3 moveDelta = new Vector3(xDelta, 0, 0);
+		transform.position += moveDelta;
+	}
+
+	private void OnEnable()
+	{
+		inputAction.Enable();
+        inputAction.Player.Move.performed += OnMove;
+		inputAction.Player.Move.canceled += OnMoveCanceled;
+		inputAction.Player.Jump.performed += OnJump;
+	}
+
+	private void OnDisable()
+	{
+        inputAction.Player.Move.performed -= OnMove;
+		inputAction.Player.Move.canceled -= OnMoveCanceled;
+		inputAction.Player.Move.performed -= OnJump;
+		inputAction.Disable();
+	}
+
+	public void OnMove(InputAction.CallbackContext context)
+	{
+		Debug.Log($"MOOVIIINNGG!!");
+		
+		xDelta = context.ReadValue<Vector2>().x * moveSpeed * Time.deltaTime;
+	}
+
+	public void OnMoveCanceled(InputAction.CallbackContext context)
+	{
+		Debug.Log($"MOOVEE CANCEELEEDD!!");
+
+		xDelta = 0;
+	}
+
+	public void OnJump(InputAction.CallbackContext context)
+	{
+		Debug.Log("JUUMMPIIINNG!!");
+
+		if (isOnGround)
+			rigidBody.velocity += new Vector2(0, context.ReadValue<Vector2>().y * jumpStrength);
+	}
+     
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if(collision.gameObject.CompareTag("Ground"))
+        {
+			isOnGround = true;
+			yDelta = 0;
+            Debug.Log("Player has landed on ground.");
+        }
+	}
+
+	private void OnCollisionExit2D(Collision2D collision)
+	{
+        isOnGround = false;
+        Debug.Log("Player has left the ground.");
+	}
+}
